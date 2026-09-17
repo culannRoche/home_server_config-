636dfee6c8fadb62f01fc7290e5c0e7efceacde8
culannRoche <culannroche@gmail.com>
2026-09-17 16:10:13 +0100

    Initial commit

---
+++ bootstrap.sh
@@ -0,0 +1,7 @@
+	##	SERVER SETUP SCRIPT	##
+
+# This script is designed to setup a Debian server that containerises services using docke
+
+# It is designed to be executed on a fresh Debian install with the ssh server and standard system utilities options
+
+
